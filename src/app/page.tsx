e39fc33505fb7fa80bbd436f66b47599fd3c72ed
JoseNeto5689 <jose.sicupiraneto@gmail.com>
2023-11-06 14:08:55 -0300

    /game route created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-import Image from 'next/image'
+"use client"
+
+import { Button } from "@mui/material"
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter()
   return (
-    <>Projeto limpo</>
+    <div className="h-screen flex flex-col justify-evenly ">
+      <header className="flex content-center items-center" >
+        <h1 className="text-center w-full text-6xl p-8" >ADS-Dungeons</h1>
+      </header>
+      <main className="w-full flex place-content-center" >
+        <Button className="mx-auto px-16 py-4 rounded-full text-lg bg-gradient-to-tr from-green-500 to-green-800" variant="contained" onClick={() => { router.replace("/game") }}>Jogar</Button>
+      </main>
+    </div>
   )
 }
