0a5f36df89646a23be31887c7c8fc606a165351e
João Victor <joao151576@gmail.com>
2023-11-18 15:08:43 -0300

    turns implemented

--- src/app/game/battle/components/Enemy.tsx
+++ src/app/game/battle/components/Enemy.tsx
@@ -1,8 +1,8 @@
 import { LinearProgress } from "@mui/material";
 import Image from "next/image";
 
-export default function Enemy({type, life}: {type: "Skeleton" | "Zombie" | "Orc" | "Slime", life: number}){
-    return <div className="w-72">
+export default function Enemy({type, life, handleClick}: {type: "Skeleton" | "Zombie" | "Orc" | "Slime", life: number, handleClick: Function}){
+    return <div className="w-72" onClick={() => {handleClick()}}>
         <Image alt="Enemy" src={"/"+type+".png"} width={390} height={390} />
         <LinearProgress variant="determinate" color="success" value={life} className="mx-3 mt-3"/>
     </div>
--- src/app/game/battle/page.tsx
+++ src/app/game/battle/page.tsx
@@ -10,14 +10,13 @@ import { timeout } from "@/app/utils";
 //Criar useEffect para verificar quem pode agir, e permitir escolher ação caso seja um player
 
 //@ts-ignore
-const mock: { players: { id: number, name: string, mana: number, vida: number, isPlayable: boolean, side: "P" }[], enemies: { id: number, type: "Skeleton" | "Zombie" | "Orc" | "Slime", vida: number, isPlayable: boolean, side: "E" }[] } = {
+const mock: { players: { id: number, name: string, mana: number, vida: number, side: "P" }[], enemies: { id: number, type: "Skeleton" | "Zombie" | "Orc" | "Slime", vida: number, side: "E" }[] } = {
   players: [
     {
       id: 1,
       name: "Jose",
       mana: 100,
       vida: 100,
-      isPlayable: true,
       side: "P"
     },
     {
@@ -25,7 +24,6 @@ const mock: { players: { id: number, name: string, mana: number, vida: number, i
       name: "Jv",
       mana: 0,
       vida: 100,
-      isPlayable: false,
       side: "P"
     },
     {
@@ -33,36 +31,54 @@ const mock: { players: { id: number, name: string, mana: number, vida: number, i
       name: "Gabriel",
       vida: 100,
       mana: 100,
-      isPlayable: false,
       side: "P"
     }
   ],
   enemies: [
     {
-      id: 1,
+      id: 4,
       type: "Skeleton",
       vida: 100,
-      isPlayable: false,
       side: "E"
     },
     {
-      id: 2,
+      id: 5,
       type: "Zombie",
       vida: 100,
-      isPlayable: false,
       side: "E"
     },
     {
-      id: 3,
+      id: 6,
       type: "Orc",
       vida: 100,
-      isPlayable: false,
       side: "E"
     }
   ]
 }
 
 let type = 0
+
+const turns = [1,4,2,5,3,6]
+const isPlayable = [true, false, false, false, false, false]
+
+let actualEntity:any = null
+let lastIndex = -1
+
+
+function getEntitie(index: number):{}{
+  let result = {}
+  mock.players.forEach(item => {
+    if(item.id == index){
+      result = item
+    }
+  })
+  mock.enemies.forEach(item => {
+    if(item.id == index){
+      result = item
+    }
+  })
+  return result
+}
 
 export default function Battle() {
 
@@ -74,16 +90,10 @@ export default function Battle() {
     //Player joga
   }
 
-  function updateTurn(){
-    setTurn(turn+1)
-  }
-
-  const [turn, setTurn] = useState(0)
-
-
   const [open, handleClickOpen, handleClose] = useDialogHooks()
 
   const [lastAction, setLastAction] = useState("")
+  const [lastTarget, setLastTarget] = useState(-1)
 
   let [dialogText, setDialogText] = useState("")
 
@@ -106,14 +116,45 @@ export default function Battle() {
         await timeout(3)
         //Mostrar inimigos
         await writeWithDelay("3 2 1", 1)
-        await timeout(1)
-        setDialogText("Escolha a sua próxima ação")
-        await timeout(3)
-        handleClickOpen()
+        await timeout(2)
+        type = 2
+        setDialogText("")
       })()
     }
     else if(type == 2){
-      setDialogText("Agora escolha um inimigo para atacar")
+      lastIndex = -1
+      turns.forEach((item,index) => {
+        if(isPlayable[index]){
+          lastIndex = index
+          actualEntity = getEntitie(turns[index])
+        }
+      })
+      if(actualEntity != null){
+        if(lastIndex+1 == turns.length){
+          isPlayable[lastIndex] = false
+          isPlayable[0] = true
+        }else {
+          isPlayable[lastIndex] = false
+          isPlayable[lastIndex+1] = true
+        }
+      }
+      //@ts-ignore
+      if(actualEntity.side == "P" | actualEntity.side=="E"){
+        type = 3
+        handleClickOpen()
+      }
+    }
+    else if(type == 4){
+      setDialogText("Escolha um inimigo")
+    }
+    else if(type == 5){
+      mock.enemies.forEach((item,index) => {
+        if(item.id == lastTarget){
+          mock.enemies[index].vida -= 20
+        }
+      })
+      type = 2
+      setDialogText("Deu bom")
     }
   })
 
@@ -123,9 +164,9 @@ export default function Battle() {
         className=" flex items-center justify-center gap-5"
         style={{ flex: 4 }}
       >
-        {mock.enemies.map(item => <Enemy key={item.id} type={item.type} life={item.vida} />)}
+        {mock.enemies.map(item => <Enemy handleClick={() => { type = 5; setLastTarget(item.id) }} key={item.id} type={item.type} life={item.vida} />)}
       </div>
-      <BattleDialog setAction={setLastAction} handleClose={() => {type=2; handleClose();}} open={open} attack={100} defense={50} />
+      <BattleDialog setAction={setLastAction} handleClose={() => {type=4; handleClose();}} open={open} attack={100} defense={50} />
       <DialogBox text={dialogText} />
       <div
         className="flex flex-row gap-1 w-screen justify-center items-end"
