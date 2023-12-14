bb098a97670a051715c0c444bc0386631cffee68
JoseNeto5689 <jose.sicupiraneto@gmail.com>
2023-12-14 13:14:38 -0300

    Ranking position implemented

--- src/app/ranking/components/Ranking.tsx
+++ src/app/ranking/components/Ranking.tsx
@@ -14,16 +14,18 @@ export function RankingPlayers({ rows }: { rows: { name: string, score: number }
       <Table aria-label="simple table">
         <TableHead>
           <TableRow className='rowConfig'>
+            <TableCell className='textDecoration' align='center'>Posição</TableCell>
             <TableCell className='textDecoration' align='center'>Usuario</TableCell>
             <TableCell className='textDecoration' align="center">Score</TableCell>
           </TableRow>
         </TableHead>
         <TableBody className='tableConfig'>
-          {rows.map((row) => (
+          {rows.map((row, index) => (
             <TableRow className='rowConfig'
               key={row.name}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
+              <TableCell className='scorePoints' align="center">{index + 1}</TableCell>
               <TableCell className='userNames' align='center' component="th" scope="row">{row.name}</TableCell>
               <TableCell className='scorePoints' align="center">{row.score}</TableCell>
             </TableRow>
