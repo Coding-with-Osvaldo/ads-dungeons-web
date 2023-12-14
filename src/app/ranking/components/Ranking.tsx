1c8b4834820a9906ef8ac4ed39dd6de07c513407
João Victor <joao151576@gmail.com>
2023-12-14 10:19:31 -0300

    organizing ranking page styling

--- src/app/ranking/components/Ranking.tsx
+++ src/app/ranking/components/Ranking.tsx
@@ -6,27 +6,26 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import './style.css'
 
-export function RankingPlayers({rows}:{rows: {name: string,score: number}[]}) {
+export function RankingPlayers({ rows }: { rows: { name: string, score: number }[] }) {
   return (
-    <TableContainer component={Paper} sx={{with: "400px"}} style={{ width: "50%" }}>
+    <TableContainer component={Paper} sx={{ with: "400px" }} style={{ width: "50%" }}>
       <Table aria-label="simple table">
         <TableHead>
-          <TableRow style={{ backgroundColor: "gray" }}>
-            <TableCell style={ { color: "white" } } align='center'>Usuario</TableCell>
-            <TableCell style={ { color: "white" } } align="center">Score</TableCell>
+          <TableRow className='rowConfig'>
+            <TableCell className='textDecoration' align='center'>Usuario</TableCell>
+            <TableCell className='textDecoration' align="center">Score</TableCell>
           </TableRow>
         </TableHead>
-        <TableBody>
+        <TableBody className='tableConfig'>
           {rows.map((row) => (
-            <TableRow
+            <TableRow className='rowConfig'
               key={row.name}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell align='center' component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="center">{row.score}</TableCell>
+              <TableCell className='userNames' align='center' component="th" scope="row">{row.name}</TableCell>
+              <TableCell className='scorePoints' align="center">{row.score}</TableCell>
             </TableRow>
           ))}
         </TableBody>
--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -1,20 +1,24 @@
 import { Button } from "@mui/material"
 import { RankingPlayers } from "./components/Ranking"
 import Link from "next/link"
+import '../ranking/components/style.css'
 export default async function Ranking() {
-    const response = await fetch("https://ads-dungeons-api.onrender.com/usuarios",{
+    const response = await fetch("https://ads-dungeons-api.onrender.com/usuarios", {
         cache: "no-cache"
     })
     const data = await response.json()
-    const mock:{name: string, score: number}[] = [...data.map((user:any) => ({name: user.nome, score: user.score}))]
+    const mock: { name: string, score: number }[] = [...data.map((user: any) => ({ name: user.nome, score: user.score }))]
 
     return <>
-        <Link href={"/"}>
-            <Button className="px-16 py-4 rounded-full text-lg bg-gradient-to-tr from-green-500 to-green-800 m-5" variant="contained">Voltar</Button>
-        </Link>
-        <h1 className="text-center pb-10 text-xl" >Ranking dos melhores jogadores</h1>
-        <div className="flex justify-center" >
-            <RankingPlayers rows={mock}/>
-        </div>
+        <body className="body">
+
+            <Link href={"/"}>
+                <Button className="px-16 py-4 rounded-full text-lg bg-gradient-to-tr from-green-500 to-green-800 m-5" variant="contained">Voltar</Button>
+            </Link>
+            <h1 className='titleRanking pb-10 text-xl' >Ranking dos melhores jogadores</h1>
+            <div className="flex justify-center" >
+                <RankingPlayers rows={mock} />
+            </div>
+        </body>
     </>
 }
